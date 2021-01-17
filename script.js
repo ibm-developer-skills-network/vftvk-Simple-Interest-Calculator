ed97e8f616172533101853418d20d18ff3c52e79
Nidhi <nidhipansuriya17@gmail.com>
2021-01-17 19:20:28 +0000

    Final changes

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector("#compute").addEventListener("click", () => {
         
+        var principal = document.querySelector("#principal").value;
+        if (int(principal) <= 0) {
+            alert("Enter a positive number");
+            document.querySelector("#principal").focus();
+        }
+
+        var rate = float(document.querySelector("#rate").value);
+        var years = int(document.querySelector("#years").value);
+        
+
+        var interest = principal * rate * years / 100;
+        var year = 2020 + years;
+        var result = 'If you deposit' + principal + ', at an interest rate of ' + rate + '%. You will receive an amount of ' + interest + ', in the year ' + year;
+        document.querySelector('#result').innerhtml = result;
+        
+    });
+
+    document.querySelector("#rate").addEventListener("input", () => {
+        var val = document.querySelector('#rate').value;
+        document.querySelector('#int_val').innerhtml = val + '%';
+    });
+});
