2d9039cb5503115757860697f5f7318960712c06
therajvarma <69708665+therajvarma@users.noreply.github.com>
2021-01-18 22:45:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    q = document.getElementById("rate").value;
-    r = document.getElementById("years").value;
-    s=p*q*r / 100
-    
+function rateValue(val){
+	document.getElementById('rateInput').value=val;
 }
+function calculate(){
+	/* déclaration des variables*/
 amount = document.getElementById('amount').value;
 rateInput= document.getElementById('rateInput').value;
 selectId=document.getElementById('selectId').value;
@@ -13,13 +10,15 @@ resultat=document.getElementById('resultat');
 
 /* calcul du nombre d'année*/
 var today= new Date();
-var n1 = today.getFullYear();
-var result1=((n1=+selectId));
+var annee = today.getFullYear();
+var result1=((annee=+selectId));
 var today2=new Date();
-var n2 =today2.getFullYear();
-var result2=result1+n2
+var annee2 =today2.getFullYear();
+var result2=result1+annee2
+
 /* calul et affichage de l'intérêt*/
-result.innerHTML="If you deposit"+"&nbsp;"+(amount)+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+(rateInput)+"."+"<br/>"+
-"You will receive an amount of"+"&nbsp;"+(s)+","+"<br/>"+
+resultat.innerHTML="If you deposit"+"&nbsp;"+(amount)+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+(rateInput)+"."+"<br/>"+
+"You will receive an amount of"+"&nbsp;"+(amount*rateInput*selectId/100)+","+"<br/>"+
 "in the year"+"&nbsp;"+(result2);
-}        
+}
+
