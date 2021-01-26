f4564510dcea1a9d251bbb55de9260f4b83d0c58
RickyCow <73363993+RickyCow@users.noreply.github.com>
2021-01-25 21:30:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = principal * years * rate / 100;
+    let d = new Date().getFullYear();
+    if (principal < 1) {
+        window.alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else {
+        let new_year = parseInt(d) + parseInt(years);
+        document.getElementById("result").innerHTML =`If you deposit <mark>${principal}</mark>,<br/>at an interest rate of <mark>${rate}</mark>,<br/>You will receive an amount of <mark>${interest}</mark>,<br/>in the year <mark>${new_year}</mark>`;
+    }
 }
-        
+
+function show_value(value) {
+    document.getElementById("slider").innerHTML = value.toString() + "%";
+}
