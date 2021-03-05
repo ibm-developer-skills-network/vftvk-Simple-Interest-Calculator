e50ce9c31b58da93af48972c6ab8f9aa1d8abeb0
Jesuss11 <80094685+Jesuss11@users.noreply.github.com>
2021-03-05 17:29:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var a = document.getElementById("principal").value;
+    if (a<=0 || a==""){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
     
+    var i = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var d = new Date();
+    var x = d.getFullYear() + parseInt(y);
+    var p = a * i/ 100 * y;
+    
+    document.getElementById("result").innerHTML=
+    "If you deposit <mark>" + a + "</mark>, <br>"
+    + "at an interest rate of <mark>" + i + "%</mark>. <br>"
+    + "You will receive an amount of <mark>" + Math.round(p) + "</mark>, <br>"
+    + "in the year <mark>" + x + "</mark>";
 }
         
