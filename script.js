ea2503b0e222d288e1ad9115ac33f2fc89233c6c
darraghquinn2014 <79016676+darraghquinn2014@users.noreply.github.com>
2021-03-09 15:34:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function output(Rate) {
+  document.querySelector('#Rate').value = Rate
 }
-        
+
+function calculate() {
+  var x = document.getElementById("amount").value;
+  var y = document.getElementById("Rate").value;
+  var z = document.getElementById("Years").value;
+  var totalInterest = (x * y * z / 100);
+  var currentYear = new Date().getFullYear();
+  var totalYears = +currentYear + +z;
+
+  text = "You will receive an amount of " + "</p><resultcolour>" + + totalInterest + "</resultcolour></p>" + " in the Year " + totalYears;
+  result.innerHTML = text ;
+} 
