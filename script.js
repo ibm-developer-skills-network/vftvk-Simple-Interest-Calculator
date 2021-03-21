b7359df60b17e6ab57edb65cca94eaa43de08e4f
code2expert <30803202+code2expert@users.noreply.github.com>
2021-03-21 16:12:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("myRange").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100;
+    var dueyear = new Date().getFullYear();
+	var myyr = Number(dueyear) + Number(years);
+
+    var displayresult = document.getElementById("result");
+    displayresult.innerText =  "If you deposit " + principal + "\n at an interest rate of " + rate + "%. \nYou will receive an interest of " + interest + "\nin the year " + myyr;
 }
-        
+
+function getmyslider(){
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    
+    output.innerHTML = slider.value + "%";
+	
+}
+       
