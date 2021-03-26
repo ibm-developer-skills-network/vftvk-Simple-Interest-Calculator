3a0be01bd774f56450e35847070b40a26f930870
priyataware9 <77295426+priyataware9@users.noreply.github.com>
2021-03-25 23:23:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function compute()
+function compute(principal,rate,year)
 {
-    p = document.getElementById("principal").value;
-    
+  var principal= document.getElementById("principal").value;
+  var p= parseInt(document.getElementById("principal").value)
+  var rate= document.getElementById("rang").value;
+  var r= parseInt(document.getElementById("rang").value);
+  var year=document.getElementById("years").value;
+  var y= parseInt(document.getElementById("years").value);
+  
+    var interest = principal*rate*year/100;
+ document.getElementById("p").innerHTML=p
+  document.getElementById("r").innerHTML=r
+  document.getElementById("result").innerHTML=interest
+  document.getElementById("y").innerHTML=2021+y
 }
-        
+function show_value(x)
+{
+document.getElementById("slider_value").innerHTML=x
+}
+
