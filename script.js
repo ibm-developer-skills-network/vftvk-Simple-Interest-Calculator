69fd5455342b5101a2e126297dca7c57ab9babde
SagarikaJaywant <sagarikajaywant015@gmail.com>
2021-03-28 15:47:36 +0000

    Changes1 in final project

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (principal * years * rate / 100);
+    var yr = parseInt(new Date().getFullYear())+parseInt(years);        
+
+    document.getElementById("result").innerHTML = "If you deposit "+principal+" at an interest of "+rate+"You will receive an amount of "+interest+"in the year "+yr;
+
+}
+
+function dispRange(val){
+    document.getElementById("sp1").innerHTML = val;//document.getElementById("range").value;  
 }
         
