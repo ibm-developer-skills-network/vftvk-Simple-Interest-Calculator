e64d4e829559b72c91c0879bb4f21896460b2165
SagarikaJaywant <sagarikajaywant015@gmail.com>
2021-03-29 10:55:57 +0000

    Final Project

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+
+    //Validating principal input box
+    if(principal<=0){
+        alert("Enter a positive value");
+        document.getElementById("principal").focus();
+        return false;
+    }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = (principal * years * rate / 100);
     var yr = parseInt(new Date().getFullYear())+parseInt(years);        
 
-    document.getElementById("result").innerHTML = "If you deposit "+principal+" at an interest of "+rate+"You will receive an amount of "+interest+"in the year "+yr;
+    //Displaying the result
+    document.getElementById("result").innerHTML = "<br>"+"If you deposit <mark>"+principal+"</mark>,"+"<br>"+"at an interest of <mark>"+rate+"%</mark>."+"<br>"+"You will receive an amount of <mark>"+interest+"</mark>,"+"<br>"+"in the year <mark>"+yr+"</mark>";
 
 }
 
+//Display numeric value of range
 function dispRange(val){
     document.getElementById("sp1").innerHTML = val;//document.getElementById("range").value;  
 }
