fb4e7e56c45437efd126413eb1ceeb2f588f1a49
easydatadev <81677209+easydatadev@users.noreply.github.com>
2021-03-31 11:50:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    r = p * r/100 * y
     
 }
         
