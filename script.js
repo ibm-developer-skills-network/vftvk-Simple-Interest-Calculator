b55c4b2a6a08c8dab78b75da64086475c2c2e0d5
Kevin Ochieng <kochieng76@gmail.com>
2021-04-05 20:46:11 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("Rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate/100;
+    
+    
     
 }
-        
+ function Years()
+ {
+     if(years<=10)
+    {
+        return years;
+    }
+} 
+  function myRate()
+  {
+      document.getElementById("range").value;
+  }    
