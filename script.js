2b482971ccc443100a4cf393d7bfbc2353715b41
Kevin Ochieng <kochieng76@gmail.com>
2021-04-11 23:04:52 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+var principal = 0;
+var rate = 0;
+var years = 0;
+var interest = 0;
+
 function compute()
 {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("Rate").value;
     var years = document.getElementById("years").value;
     var interest = principal*years*rate/100;
-    
-    
-    
+    document.getElementById("principal").innerHTML = "If you deposit";
+    document.getElementById("Rate").innerHTML = "at an interest rate of";
+    document.getElementById("interest").innerHTML = "You will receive an amount of ";
+    document.getElementById("years").innerHTML = "in the year";
 }
  function Years()
  {
@@ -17,5 +23,7 @@ function compute()
 } 
   function myRate()
   {
-      document.getElementById("range").value;
+      event.getElementById("range").value;
   }    
+ 
+  
