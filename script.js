f01facc27c2def40754bae94152315a38d07ee08
adika king <ask.sunn@live.com>
2021-04-16 07:49:53 +0000

    calculate function working

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText = rateval;
 }
 
 function compute()
@@ -9,8 +9,14 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
+var interest = (principal * years * rate) /100;
     var year = new Date().getFullYear()+parseInt(years);
+
+document.getElementById("result").innerHTML = "if you deposit: " + principal + '\n' + 
+"at an interest rate of: " + rate + '\n' +
+"You will receive an interest of " + interest + '\n' +
+"At the end of " + year
+    
     
 }
         
