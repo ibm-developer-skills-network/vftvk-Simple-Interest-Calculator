9144f458f58d470cdf2a01db0f7e22e4e56cb598
Friendly Silva <friendlysilva7746@gmail.com>
2021-04-18 13:46:36 +0000

    changes by friendly silva

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
- function compute(){
- p = document.getElementById("principal").value;
 
+function compute (){
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-    var year =  year.getFullYear()+parseInt(years);
- }
+    var year =  new Date().getFullYear()+parseInt(years);
+    
+    }
+ 
+function updateRate()
+     {var rateval = document.getElementById("rate").value;
+     document.getElementById("rate_val").innerText=rateval;
+}
+document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 
-     document.getElementById("result").innerHTML = "interest";
+          function myFunction(rate_v) {
+                                 document.getElementById("rate").innerHTML = rate_val;
+
+                             }
