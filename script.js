6c6783c566eca5d5f5c58b72790945fe7a4d77fb
Friendly Silva <friendlysilva7746@gmail.com>
2021-04-18 11:11:40 +0000

    changes from friendly  silva

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+ function compute(){
+ p = document.getElementById("principal").value;
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year =  year.getFullYear()+parseInt(years);
+ }
+
+     document.getElementById("result").innerHTML = "interest";
