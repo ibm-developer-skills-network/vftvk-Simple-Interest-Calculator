eb06a00df0ff1ef7af2161654ba967c2a02c1e10
EduSanG <82851756+EduSanG@users.noreply.github.com>
2021-04-21 11:06:37 -0700

    Update script.js
    Assignment mods

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
-}
+    var principal = document.getElementById("principal").value;
+
+    if(principal<=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }else{
+
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear() + parseInt(years);
         
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br>at an interest rate of <mark>" + rate +"%</mark><br>You will receive an amount of <mark>" + interest + "</mark>,<br>In the year <mark>" + year + "</mark>";
+    }
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
