ddb71cb3ca0091b71d24b62863ede7321aa3b47b
RichieSK <81282315+RichieSK@users.noreply.github.com>
2021-04-24 19:39:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * rate * years/100;
+    years = 2021 + parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit " + principal.toString();
+    document.getElementById("result").innerHTML+="<br>at an interest rate of " + rate.toString();
+    document.getElementById("result").innerHTML+="%.<br>You will receive an amount of " + interest.toString();
+    document.getElementById("result").innerHTML+=",<br>in the year " + years.toString();
     
+    return interest;
 }
         
