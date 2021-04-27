6529aede9764bb12fe7ad8531a21833c9874c1e6
osoman2 <osman.vilchez@utec.edu.pe>
2021-04-27 05:17:44 +0000

    js

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+
+function validate(){
+    var principal = document.getElementById("principal").value;
+    if (principal=="" || principal<=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }     
+    return true;
+}
+
 function compute()
 {
   
-   
+   validate();
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
