f902a40588c8c1a4bd367e71037f8abd9681c690
osoman2 <osman.vilchez@utec.edu.pe>
2021-04-27 05:26:15 +0000

    end

--- script.js
+++ script.js
@@ -18,13 +18,16 @@ function validate(){
 function compute()
 {
   
-   validate();
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var amount = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
+   if(validate()){
 
-  document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
+
+       var principal = document.getElementById("principal").value;
+       var rate = document.getElementById("rate").value;
+       var years = document.getElementById("years").value;
+       var amount = principal * years * rate /100;
+       var year = new Date().getFullYear()+parseInt(years);
+       
+       document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
+    }
 }
         
