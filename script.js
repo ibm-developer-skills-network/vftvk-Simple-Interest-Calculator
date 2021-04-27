42792b4054e82dd1217afbe6f146b54235a8b227
Osvaldo Molina <osva64ko@gmail.com>
2021-04-27 07:18:59 +0000

    Changes for activity

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function updateRate() {
+    // refresh rate when range is changed
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + '%';
 }
-        
+
+function compute() {
+    var principal = document.getElementById("principal").value;
+
+    // check if the ammount is valid
+    if(principal <= 0) 
+    {
+        alert("Please enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    // calculate interest
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    // show result
+    document.getElementById("result").innerHTML="If you deposit <b>"+principal+"</b>,\<br\>at an interest rate of <b>"+rate+"</b>.\<br\>You will receive an amount of <b>"+amount+"</b>,\<br\>in the year <b>"+year+"</b>\<br\>"
+}
+
