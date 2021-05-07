0c8e80cad65222d18001d056d755f7149469d237
Raul Mayobre <raulma486@hotmail.com>
2021-05-07 00:53:18 +0000

    By Raul V1

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+
+    var principal = document.getElementById("principal").value;
+
+    var rate = document.getElementById("rate").value;
+
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate /100;
+
+      var year = new Date().getFullYear()+parseInt(years);
     
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
