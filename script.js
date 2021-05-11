af24252b5126a9ebd3eeacec4cfed3d792571320
chandu-21 <84000771+chandu-21@users.noreply.github.com>
2021-05-11 10:50:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+body {background-color:black;font-family:arial;color:white}
+h1{color:grey;font-family:verdana}
+.maindiv {
+    background-color:white;
+    color:black;
+    width:300px;
+    padding:20px;
+    border-radius:25px;
+    align:center
 }
         
