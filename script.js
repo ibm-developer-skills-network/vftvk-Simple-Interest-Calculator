6a56286142903b3ebcfc91bb92cd2704b447c543
Juan Pedro Serna <jpcserna@gmail.com>
2021-05-14 00:46:56 +0200

    Completed tasks. Full version.

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ slider.oninput = function() {
 
 function compute()
 {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value; 
-    alert(p + r + y); 
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var total = ((p*r)/100)*5; 
+    printResult(p, r, y, total);
 }
+
+function printResult(prin,rat, yea, tot)
+{
+  var currentYear = new Date().getFullYear();
+  var receiveYear = parseInt(currentYear) + parseInt(yea);
+  document.getElementById("textresults").style.visibility = "visible";
+  document.getElementById("deposit").innerHTML = prin;
+  document.getElementById("interestrate").innerHTML = rat +"%";
+  document.getElementById("receiveamount").innerHTML = tot;
+  document.getElementById("receiveyear").innerHTML = receiveYear;
+}
+
