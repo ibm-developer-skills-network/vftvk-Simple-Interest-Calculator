bd981686ae9903009f58ac0842e9959bc68f2a38
Juan Pedro Serna <jpcserna@gmail.com>
2021-05-13 02:02:17 +0200

    Changed html, js, css

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+var slider = document.getElementById("rate");
+var output = document.getElementById("ratenumber");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
 function compute()
 {
     p = document.getElementById("principal").value;
     
 }
+
+
         
