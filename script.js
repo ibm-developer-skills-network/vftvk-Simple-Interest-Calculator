859918c7876812f17123a6ea3f32b5f246bd30fb
Juan Pedro Serna <jpcserna@gmail.com>
2021-05-13 14:41:26 +0200

    Changed html, js, css

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var slider = document.getElementById("rate");
-var output = document.getElementById("ratenumber");
+var slider = window.document.getElementById("rate");
+var output = window.document.getElementById("ratenumber");
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
-
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value; 
+    alert(p + r + y); 
 }
-
 
         
