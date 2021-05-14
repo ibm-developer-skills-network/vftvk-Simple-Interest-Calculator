43bc725f80c813c24c7a9e649585bdfd2ca25183
Juan Pedro Serna <jpcserna@gmail.com>
2021-05-14 02:13:34 +0200

    Added improvements for graded compliance

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+
+
 var slider = window.document.getElementById("rate");
 var output = window.document.getElementById("ratenumber");
 output.innerHTML = slider.value;
+
+document.getElementById("compute").addEventListener("click", function() {
+  var x = document.getElementById("principal").value;
+  var y = document.getElementById("years").value;
+  if (isNaN(x) || x < 1) {        //Principal validation
+    alert('You must enter a positive number as PRINCIPAL!');
+    document.getElementById("principal").focus();
+  } else if (isNaN(y) || y < 1) {
+    alert('You must enter a positive number as YEARS!');
+    document.getElementById("years").focus();
+  } else {
+    compute();
+  }
+});
 
 slider.oninput = function() {
   output.innerHTML = this.value;
@@ -11,7 +27,7 @@ function compute()
     var p = document.getElementById("principal").value;
     var r = document.getElementById("rate").value;
     var y = document.getElementById("years").value;
-    var total = ((p*r)/100)*5; 
+    var total = ((p*r)/100)*y; 
     printResult(p, r, y, total);
 }
 
@@ -25,4 +41,3 @@ function printResult(prin,rat, yea, tot)
   document.getElementById("receiveamount").innerHTML = tot;
   document.getElementById("receiveyear").innerHTML = receiveYear;
 }
-
