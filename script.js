5ce65544876a3cc3bdb1513461457acdc0cd8f77
Kevin-Oudai <kevinoudai@hotmail.com>
2021-05-13 21:18:31 -0400

    Update Index.html, script.js, style.css

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    console.log("Hi");
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
     
+    var result = "If you deposit " + principal;
+    result += "\n at an interest of " + rate;
+    result += "%.\n You will receive an amount of " + interest;
+    result += "\nin the year " + year;
+    document.getElementById("result").innerText = result;
+    console.log(result);
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
         
