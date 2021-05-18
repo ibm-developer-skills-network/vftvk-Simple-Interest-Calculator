d4dbec0b535bc2001709174ad165428e905642b3
sandhya05 <sandhyamohan90@gmail.com>
2021-05-18 13:46:02 +0800

    js file update

--- script.js
+++ script.js
@@ -11,16 +11,14 @@ function updateRate()
  
 }
 
-// function compute()
-// {
-//     var principal = document.getElementById("principal").value;
-//     var rate = document.getElementById("rate_val").value;
-//     var years = document.getElementById("years").value;
-//     var year = new Date().getFullYear()+parseInt(years);
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate_val").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
 
-//     var amount = principal * years * rate /100;
-//     document.getElementById("result").innerText=amount
-//     document.getElementById("result").innerText="amount";
-//     // document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-// }
+    var amount = principal * years * rate /100;
+document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+}
         
