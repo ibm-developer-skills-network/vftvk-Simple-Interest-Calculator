aced46f506cc716c7800f2ad99e6ba955e2ab440
sandhya05 <sandhyamohan90@gmail.com>
2021-05-18 14:01:47 +0800

    js file update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function updateRate()
 {   console.log("called");
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-    var rateval = document.getElementById("rate_val").value;
+    // var rateval = document.getElementById("rate_val").value;
  
 }
 ı
