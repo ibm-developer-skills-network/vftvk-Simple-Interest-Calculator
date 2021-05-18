48210b11283bbfc04669b355a8ddd737a6590703
sandhya05 <sandhyamohan90@gmail.com>
2021-05-18 13:13:53 +0800

    Javascript file update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function updateRate()
 
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var year = new Date().getFullYear()+parseInt(years);
