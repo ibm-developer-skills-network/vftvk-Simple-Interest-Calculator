b03874c0e6d02470a844413772d28a63f7b0956e
sandhya05 <sandhyamohan90@gmail.com>
2021-05-18 21:43:30 +0800

    Updated js file as per requirements

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compute(){
 
     var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="If you deposit "+<b>principal</b>+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
 
 function updateRate() 
