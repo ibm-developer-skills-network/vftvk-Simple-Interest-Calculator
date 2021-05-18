ef2dbd9f9d1bff2eff90ba0df6d2afb040ba3e3f
sandhya05 <sandhyamohan90@gmail.com>
2021-05-18 13:07:22 +0800

    Javascript file update

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
 
-var year = new Date().getFullYear()+parseInt(years);
+
+
+
 
 function updateRate() 
-{
+{   console.log("called");
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
@@ -14,6 +12,11 @@ function updateRate()
 function compute()
 {
     p = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    var interest = principal * years * rate /100;
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
     
 }
