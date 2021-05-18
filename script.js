1f50398a0eeeab5ae36c83f2cd9bc1e54ef920c6
sandhya05 <sandhyamohan90@gmail.com>
2021-05-18 13:43:31 +0800

    js file update

--- script.js
+++ script.js
@@ -5,20 +5,22 @@
 
 function updateRate() 
 {   console.log("called");
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    // var rateval = document.getElementById("rate").value;
+    // document.getElementById("rate_val").innerText=rateval;
+    var rateval = document.getElementById("rate_val").value;
+ 
 }
 
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate_val").value;
-    var years = document.getElementById("years").value;
-    var year = new Date().getFullYear()+parseInt(years);
+// function compute()
+// {
+//     var principal = document.getElementById("principal").value;
+//     var rate = document.getElementById("rate_val").value;
+//     var years = document.getElementById("years").value;
+//     var year = new Date().getFullYear()+parseInt(years);
 
-    var amount = principal * years * rate /100;
-    document.getElementById("result").innerText=amount
-    document.getElementById("result").innerText="amount";
-    // document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-}
+//     var amount = principal * years * rate /100;
+//     document.getElementById("result").innerText=amount
+//     document.getElementById("result").innerText="amount";
+//     // document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+// }
         
