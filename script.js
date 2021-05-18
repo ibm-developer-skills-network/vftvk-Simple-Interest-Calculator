fcce88e38193b372723db92671c30e9b034f0744
sandhya05 <sandhyamohan90@gmail.com>
2021-05-18 20:14:30 +0800

    Updated js file as per requirements

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function compute(){
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value
-    var interest=principal * years * rate /100;
+    var amount=principal * years * rate /100;
 
     var year = new Date().getFullYear()+parseInt(years);
 
