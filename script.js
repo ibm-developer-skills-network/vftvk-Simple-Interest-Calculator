8f792949cadf11f94f406a8d63ee2ebf76a0e57a
yxtao <yinxiatao@gmail.com>
2021-05-24 15:44:17 -0700

    Fix style

--- script.js
+++ script.js
@@ -28,5 +28,5 @@ function compute()
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
