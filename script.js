83d7c9b5c8e939737e0abd1c19e0cde20ef34501
Veli Can YILDIZ <velicanyildiz@outlook.com>
2021-05-29 02:28:24 +0300

    Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {    
+    var principal = document.getElementById("principal");
+    if(principal.value <= 0){
+            alert("Please Enter a Positive Number!");
+            principal.focus();
+            return false;
+    } else
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit <strong>"+principal+"</strong>,<br>at an interest rate of <strong>"+rate+"%</strong>.<br>You will receive an amount of <strong>"+interest+"</strong>,<br>In the year <strong>"+year+"</strong>.<br>";
 }
-        
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
