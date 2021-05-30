9d1c6d510a2e4dfd021e0cb16cfb866c3e349c27
Thiago Penteado <thiago.penteado@d0007azurecloudtotvscom.onmicrosoft.com>
2021-05-30 15:12:46 -0300

    feat: 🎸 update and finalize final exam

--- script.js
+++ script.js
@@ -7,21 +7,15 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var amount = principal * years * rate /100;
 
-    //set validate variable
-    var validate = true;
-
-    // Call validateDate
-    validateDate(year);
-
-    //validation for "Principal"
-    validationPrincipal(principal);
+   // validation for "Principal" and "Year"
+    var validate = validateFunction(year, principal);  
 
     //IF validate is true, continue
     if(validate){
         // Get the reference to the element named 'result' and upadte when "Compute Interest" is clicked
         document.getElementById("result").innerHTML="If you deposit "
             +"<span class='highlightedNumbers'>"+principal+ "</span>,\<br\>at an interest rate of "
-            +"<span class='highlightedNumbers'>"+rate+"%</span> \<br\>You will receive an amount of "
+            +"<span class='highlightedNumbers'>"+rate+"%</span>. \<br\>You will receive an amount of "
             +"<span class='highlightedNumbers'>"+amount+"</span>,\<br\>in the year "
             +"<span class='highlightedNumbers'>"+year+"</span> \<br\>";
     }
@@ -31,27 +25,22 @@ function updateRate()
 {
     // set Span rate_val
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
 
-function validateDate(year) {
+function validateFunction(year, principal) {
     // function to validate year
-    if (isNaN(year)) {
-        alert('the year Not a Number!');
-        document.getElementById("years").focus();
-        validate = false;
+    if ((isNaN(year)) || (principal <= 0)) {
+        if (isNaN(year)) {
+            alert('the year Not a Number!');
+            document.getElementById("years").focus();
+        } 
+        if (principal <= 0) {
+            alert('Enter a positive number');
+            document.getElementById("principal").focus();
+        }
+        return false;
     } else {
-        validate = true;
-    }
-}
-
-function validationPrincipal(principal){
-    // validation for "Principal"
-    if (principal <= 0) {
-        alert('Enter a positive number');
-        document.getElementById("principal").focus();
-        validate = false;
-    } else {
-        validate = true;
+        return true;
     }
 }
