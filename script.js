da36789729a4c21ee5b028e29d03f86572cfc0dd
Thiago Penteado <thiago.penteado@d0007azurecloudtotvscom.onmicrosoft.com>
2021-05-30 14:06:00 -0300

    feat: 🎸 correct year validation function

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    
+    // comment old (var interest = principal * years * rate /100;)
 
     // Call validateDate
     validateDate(year);
@@ -34,12 +35,9 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval;
 }
 
-function validateDate(date) {
-    // RegEx to validate Date
-    var regEx = /^((((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])      [\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))|(((0[1-9]|[12]\d|3[01])(0[13578]|1[02])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|[12]\d|30)(0[13456789]|1[012])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|1\d|2[0-8])02((1[6-9]|[2-9]\d)?\d{2}))|(2902((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00))))$/;
-    if (!(date.match(regEx))) {
-        alert('Invalid date!');
-    }
+function validateDate(year) {
+    // function to validate year
+    console.log(year);   
 }
 
 function validationPrincipal(principal){
