dfb0a193250d686234b65b6087054f3e0f6c04c5
shanukova <vshanukova@gmail.com>
2021-06-08 16:32:42 +0000

    Improve design & functionality of Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+
+
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear() + parseInt(years); 
+    var interest = principal * years * rate /100; 
+
+    if (principal <= 0) {
+        alert('Enter a positive number');
+        document.getElementById("principal").focus();
+        return;
+    }
+
+
+    document.getElementById("result").innerHTML = "If you deposit <span>"
+    + principal + "</span>,\<br\>at an interest rate of <span>" 
+    + rate + "%</span>\<br\>You will receive an amount of <span>"
+    + interest + "</span>,\<br\>in the year <span>"
+    + year + "</span>\<br\>";
 }
-        
+  
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
+
+document.getElementById("rate").onchange = updateRate;
+
+
+
