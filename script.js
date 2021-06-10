8089dd2bc3cf0af16d0d2709f4f92454b7e09f7c
gokulgd7 <85565513+gokulgd7@users.noreply.github.com>
2021-06-10 23:11:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = (principal*years*rate)/100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+<br/>+
-    "at an interest rate of "+rate+<br/>+"You will receive an amount of "+interest+<br/>+"in the year "+year;
+    document.getElementById("result").innerHTML="If you deposit "+principal+"<br/>"+
+    "at an interest rate of "+rate+"%"+"<br/>"+"You will receive an amount of "+interest+"<br/>"+"in the year "+year;
 
-    
+   return false; 
 }
 function updateRate()
 {
     var rateval=document.getElementById("rate").value;
-    document.getElementById("rate_val").innerHTML=rateval;
+    document.getElementById("rate_val").innerHTML=rateval+"%";
 }
         
