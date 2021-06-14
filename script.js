63de09b8f2a86976b9627af3293ea70bbc941e84
Greg <84861511+gkutyn@users.noreply.github.com>
2021-06-14 08:57:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,6 @@ function compute()
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
         
