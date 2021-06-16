80e71b524d5bec90fbfa1ea69e3775679d90d4f4
Greg <84861511+gkutyn@users.noreply.github.com>
2021-06-15 22:32:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,24 @@
 
 function compute()
 {
+    /* Get input values */
     var principal = parseInt(document.getElementById("principal").value);
     var rate = parseInt(document.getElementById("rate").value);
     var years = parseInt(document.getElementById("years").value);
 
+    /* Calculate Interest */
     var interest = principal * years * rate /100;
     var ppint = principal + interest; // Principal plus interest
 
+    /* Calculate period end year */
     var year = new Date().getFullYear() + parseInt(years);
     
-    var resultstr =  "<p>If you deposit " + "<span class='highlight'>" + principal + <\/span> + ",<br>";
-        resultstr += "at an interest rate of " + "<span class='highlight'>" + rate + <\/span> + "%.<br>";
-        resultstr += "You will receive an amount of " + "<span class='highlight'>" + ppint + <\/span> + ",<br>"; 
-        resultstr += "in the year " + "<span class='highlight'>" + year + <\/span> +".<br><br><\/p>";
-        
-    document.getElementById("result").innerHTML = resultstr;      
+    /* Create result string and display it */
+    vvar resultstr =  "<p>If you deposit " + "<span class='highlight'>" + principal + "<\/span>,<br>";
+        resultstr += "at an interest rate of " + "<span class='highlight'>" + rate + "<\/span> %.<br>";
+        resultstr += "You will receive an amount of " + "<span class='highlight'>" + ppint + "<\/span>,<br>"; 
+        resultstr += "in the year " + "<span class='highlight'>" + year + "<\/span>.<br><br><\/p>";
+   document.getElementById("result").innerHTML = resultstr;      
 }
 
 /* UI enhancement to improve usability
