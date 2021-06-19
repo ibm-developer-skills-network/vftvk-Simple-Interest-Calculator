2d503a30ee0f853ce10c82bc68c6b1316218ba89
Chetan030500 <c,chhaparwal@gmail.com>
2021-06-19 09:31:17 +0000

    hello chetan

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+
+//to calculate interest on click function compute
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //check for negative and zero principal value
+    var principal = document.getElementById("principal").value;
+   if(principal<=0){
+       //clear the previous result and give alert
+       document.getElementById("result").innerHTML="";
+       alert("Enter a positive number");
+       document.getElementById("principal").focus();
+
+   }
+   else{
+        
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit "+"<span class='highlight'>"+principal+"</span>"+",\<br\>at an interest rate of "+"<span class='highlight'>"+rate+"%"+"</span>"+"\<br\>You will receive an amount of "+"<span class='highlight'>"+interest+"</span>"+",\<br\>in the year "+"<span class='highlight'>"+year+"</span>"+"\<br\>"
+   }
     
+}
+//function to update span with rate slider
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
