66ecae86b21f699b768e2e417f9f0e0e3cecfb78
Vasudeva Pitta <38531141+vasudevapitta@users.noreply.github.com>
2021-06-19 12:10:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function get_interest_rate() {
+    var el = document.querySelector('#slider_val')
+    var ir_val = (parseFloat(el.value)/0.5).toFixed(2);
+    return ir_val;
 }
-        
+
+function set_interest_rate() {
+    var ir = document.querySelector('#ir_output');
+    ir.innerHTML = get_interest_rate() +'%';
+}
+
+function compute() {
+    var amount = document.querySelector('#principal').value;
+    var percentage = get_interest_rate();
+    var years = Number(document.querySelector('#years').value);
+    var estimatedYear = years + new Date().getFullYear();
+    var estimatedAmount = amount*percentage*years / 100;
+    var result = document.querySelector('#result');
+    result.innerHTML = 'If you deposit ' + amount + ',' + '<br>'
+    + 'at an interest rate of '+ percentage + '%.' + '<br>'
+    + 'You will receive an amount of '+ estimatedAmount +',' + '<br>'
+    + 'in the year ' + estimatedYear;
+}     
