1e072cfd22fac52ec7e145da2b1c9e5769b9cd24
robg916 <86701371+robg916@users.noreply.github.com>
2021-06-30 02:11:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
+{  var principal = document.getElementById("principal").value; principal; if (x= 0) alert;"Enter a positive number)"
+(focus)
+   var rate = document.getElementById("rate").value; rate
+    var years = document.getElementById("years").value; years 
+    var interest = principal * years * rate /100; value; interest
+      var year = new Date().getFullYear()+parseInt(years);year
+   <span>
+       function updateRate() 
 {
-    p = document.getElementById("principal").value;
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval; rangeinput; onchange
+}
+  {document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+      p = document.getElementById("principal").value;
     
 }
-        
+} 
