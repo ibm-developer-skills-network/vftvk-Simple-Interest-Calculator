e63b6ab69c0aa2ac37d42d99d649f8f36319d2d3
EuroCoon <46499622+EuroCoon@users.noreply.github.com>
2021-07-07 20:10:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
     
+    if (principal < "1") {
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+
+    document.getElementById("result").innerHTML =
+        "If you deposit " + principal + ",<br\>at an interest rate of " + rate + "%.<br\>You will receive an amount of " + interest +
+        ",<br\>in the year " + year;
+
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval +"%";
+}
+
