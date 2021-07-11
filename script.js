eab21e62788fbbddd069c5301ad013b0cbed34ec
Constantine Kuks <conkuk@gmail.com>
2021-07-11 18:20:39 +0200

    Solution for the final project

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  // values
+  const principal = parseInt(document.getElementById("principal").value);
+  const rate = parseFloat(document.getElementById("rate").value);
+  const years = parseInt(document.getElementById("years").value);
+
+  // Result DOM element
+  const result = document.getElementById("result");
+
+  // Calculations
+  const currentYear = new Date().getFullYear();
+  const calculatedYear = currentYear + years;
+  const calculatedAmount = principal*(rate/100)*years;
+
+  result.innerHTML = `
+    <p>
+        If you deposit ${principal},<br/>
+        at an iterest rate of ${rate}%.<br/>
+        You will receive an amount of ${calculatedAmount}.<br/>
+        in the year ${calculatedYear}
+    </p>
+`;
+
 }
-        
+
+
+window.onload = function () {
+  updateRateValue();
+}
+
+function updateRateValue() {
+  const rate = document.getElementById("rate").value;
+  document.getElementById("interestRate").innerHTML = rate + "%";
+}
