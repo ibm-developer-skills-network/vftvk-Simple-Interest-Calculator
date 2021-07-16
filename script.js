fdd1040c130aa0403df3886d24839c342473012e
RigoCeroXD <67384250+RigoCeroXD@users.noreply.github.com>
2021-07-16 11:46:30 -0700

    Add files via upload
    aqui esta

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+
+if(this.principal > 1 || this.principal == 1)
+{
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "
+    +rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+}
+
+}
+
+
+
 function compute()
 {
     p = document.getElementById("principal").value;
     
 }
-        
