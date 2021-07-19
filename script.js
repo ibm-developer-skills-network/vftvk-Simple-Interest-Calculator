782d3c572e11004329476e69ef85b423d84dc34b
fgavilasoto <fg.avilasoto@gmail.com>
2021-07-19 16:12:31 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // get elements from document
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    // calculate interest and year
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear() + parseInt(years);
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
