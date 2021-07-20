efd03f1c26ea26736e98ac6c776dc8190180890b
FGavilasoto <fg.avilasoto@gmail.com>
2021-07-20 15:05:04 -0400

    Added interest computation to app.
    Added interest calculation to javascript file.  Added event to compute button index page to trigger and display calculation.

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ function compute()
     // calculate interest and year
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear() + parseInt(years);
+
+    if (principal <= 0)
+    {
+      document.getElementById("result").innerHTML = "Please enter a positive value for amount"
+    }
+    else
+    {
+      document.getElementById("result").innerHTML = "If you deposit <span id = 'highlight'>" + principal + "</span>,<br/>at an interest rate of <span id = 'highlight'>" + rate + "%</span><br/>You will receive an amount of <span id = 'highlight'>" + interest + "</span>,<br/>in the year <span id = 'highlight'>" + year + "</span><br/>"
+    }
 }
 
 function updateRate()
