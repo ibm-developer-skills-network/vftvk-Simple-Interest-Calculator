0658699c68eb0cea3687c21fcac216f81f499250
Akal <akal>
2021-07-31 08:18:14 +0000

    Changes for final project, version 1

--- script.js
+++ script.js
@@ -5,22 +5,33 @@ function compute()
     //variables inputted by user
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years");
 
-    //Calculation of interest
-    var interest = principal*years*rate/100;
-    
-    //Date determination based on user inputted No. of years
-    var year = new Date().getFullYear()+parseInt(years);
+    //obtain No. of years and parse it into an integer
+    var years = parseInt(document.getElementById("years").value);
 
-    //Modify compute interest text
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    //Validate the Principal amount input
+    if(principal <= 0) {
+        alert('Enter a positive number');
+        document.getElementById("principal").focus();
+    }
+    //After successful validation of positive number run the calculation for simple interest
+        else {
+        //Calculation of interest
+        var interest = principal*years*rate/100;
+        
+        //Year determination based on user inputted No. of years after todays year
+        var year = new Date().getFullYear()+parseInt(years);
 
-}
+        //Modify text to show the data used for the calculation and display years
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of $"+interest+",\<br\>in the year "+year+"\<br\>"
+        }
+    }
+
 
 //obtain the rate from the range bar and insert into the element
 function updateRate() 
 {
+    //obtain the rate value from the html element
     var rateval = document.getElementById("rate").value;
 
     //insert the new rate into the html tag
