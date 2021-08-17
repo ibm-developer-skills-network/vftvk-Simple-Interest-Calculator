7bb562c28d8ab6e2d001efadb8105ebd7db36dd3
luluu123 <88992329+luluu123@users.noreply.github.com>
2021-08-16 22:42:28 -0300

    funcs

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+
+    i = p*((r/100)*y);
+    document.getElementById("result").innerHTML = "If you deposit " + p + ",<br />" + 
+    "at an interest rate of " + r + ".<br />" +
+    "You will receive an amount of " + i.toFixed(0) + ",<br />" +
+    "in the year " + y;
+    //console.log(i);
 }
-        
+
