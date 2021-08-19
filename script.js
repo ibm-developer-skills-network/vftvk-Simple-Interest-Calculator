67d76880ea3f11c137a23cffac76561a062dd7d8
Soumyajit Dawn <soumyajitdawn@gmail.com>
2021-08-19 23:37:01 +0530

    added javascript functions

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function showInt(){
+    
+    var r=document.getElementById("rate").value;
+    
+    document.getElementById("result").innerHTML=r;
     
 }
-        
+
+
+document.getElementById("rate").addEventListener("click",showInt);
+
+
+
+function showRes(){
+    var p=document.getElementById("principal").value;
+    var r=document.getElementById("rate").value;
+    var t=document.getElementById("years").value;
+    var cY=2021+parseInt(t);
+
+    var res=(p*r*t)/100;
+    document.getElementById("interestR").innerHTML="If you deposit "+p+"<br>"+"at an interest rate of "+r+"%"+"<br>"+"You will receive an amount of "+res+","+"<br>"+"in the year "+cY;
+    // alert(res);
+}
