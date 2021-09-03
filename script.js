79f57cda7afe5dd55a8d29c9d1ea1e77625a9511
R2 D2 <klaus.gibbe@t-online.de>
2021-09-03 09:32:13 +0000

    exercise done

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;   
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100.0;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+
+    document.getElementById("principal").focus();
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
+}
+
+
+function checkPositiveNumber() 
+{
+    var amount = document.getElementById("principal").value;
+    if (amount < 0)
+    {
+    	alert("Enter a positive number")
+    }
 }
         
