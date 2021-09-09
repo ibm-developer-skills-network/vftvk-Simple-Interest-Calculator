def2fa14d7d1a42de100eeef2ebbdd3ccd14e7f7
Samanna M <46653064+sam3112@users.noreply.github.com>
2021-09-09 23:50:09 +0530

    script updated
    added functionality

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+r=document.querySelector("#rate");
+r.onchange=function(){
+r.nextElementSibling.innerHTML=this.value+"%";
 }
+p=document.querySelector("#principal");
+t=document.querySelector("#years");
+var i=p.value*t.value*r.value/100;
+result=document.querySelector("#result_display");
+compute=document.querySelector("#compute");
+clear=document.querySelector("#clear");
+compute.onclick=function(){
+var y=2020+Number(t.value);
+result.innerHTML="";
+i=Math.round(p.value*t.value*r.value/100);
+
+result.innerHTML+="If you deposit <span class='yellow'>"+p.value+"</span><br>at an interest rate of <span class='yellow'>"+r.value+"</span>%<br>You will receive an amount of <span class='yellow'>"+i+"</span><br>in the year <span class='yellow'>"+y+"</span>";}
+
+clear.onclick= function(){location.reload();}
         
