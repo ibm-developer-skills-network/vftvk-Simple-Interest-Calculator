92149f13339c5b8fb32f8a74dfb7663e7f38d222
Colleen Malone <briarrose@Colleens-iMac.local>
2021-09-10 19:11:58 -0400

    editing calculator

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+function updateRate(){
+	// get and report value for percentage rate
+	var rateval = document.getElementById("rate").value
+	document.getElementById("rate_val").innerText=rateval+"%";
+
 }
-        
+
+
+
+function compute(){
+	// set variable to principal input
+	var principal = document.getElementById("principal").value;
+	
+	//validation for principal input
+	if(principal <= 0){
+	alert("Please enter a positive value");
+	document.getElementById("principal").focus();
+	return false;
+	}
+	
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+
+	var interest = principal * years * rate /100
+	var year = new Date().getFullYear()+parseInt(years);
+
+
+    p = document.getElementById("principal").value;
+    document.getElementById("interest").innerText=interest;
+    
+  
+    document.getElementById("result").innerHTML="If you deposit "+principal+",<br>at an interest rate of "+rate+"% <br>You will receive an amount of "+interest+"<br>in the year "+year;
+
+    
+    }
