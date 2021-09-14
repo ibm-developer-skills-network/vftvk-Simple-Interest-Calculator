29d956b06df091f82a97dee39f7b4f906f611e96
xxulverxx <35438607+rjrm84@users.noreply.github.com>
2021-09-14 19:57:35 -0300

    calculator complete excericise

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function compute()
+// JavaScript
+function calculate()
 {
-    p = document.getElementById("principal").value;
-    
+
+     amount= document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+     time = document.getElementById("years").value;
+    interest = document.getElementById("result");
+    result.innerHTML="The interest is " + (amount*time*rate/100);
+
 }
-        
+
+function myFunction() {
+    document.getElementById("myForm").reset();
+}
