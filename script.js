baf7b919d66670060e9f27e460a13414d067a242
Fakhreddine Yousfi <Fakhriyo@hotmail.fr>
2021-09-16 21:19:22 +0100

    commit the update to the code

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+   
+    
+    var principal = document.getElementById("principal").value;
+    if (principal<=0){
+        window.alert("Enter a positive number")
+    }else {
+        var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var textresult =  'if you deposit <mark>' +principal+ ' </mark><br> at an interest rate of <mark>' +rate+ '</mark><br> you will receive an amout of <mark>' + interest+ '</mark><br> in the year '  + year+'</div>';
+    //var textresult = '<div id="gator">Alligators rule!!</div>'
+    document.getElementById("result").innerHTML  = textresult;
+    // console.log(principal);
+    // console.log(rate);
+    // console.log(years);
+    }
+    
     
 }
-        
+function updateRate(){
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateVal;
+}    
