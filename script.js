aba6c96a47dcc40c2e1a4aa314cee538b0aa052f
Hunter Faulkner <62822086+hrflkner@users.noreply.github.com>
2021-09-17 17:42:19 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function compute() {
-    var principal = document.getElementById("principal").value;
-
+    //Name p to avoid double reference to principal id tag
+    //Otherwise the focus method won't execute properly
+    var p = document.getElementById("principal").value;
+    if (p <= 0) {
+        alert("Please enter a positive value for the principal.");
+            //Upon alert close, brings input back to principal
+            principal.focus();
+            return false;
+    }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
+    var interest = p * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
-
-    solution = "If you deposit <span class='highlight'>&#165;"+principal+"</span>\<br\>at an interest rate of <span class='highlight'>"+rate+"%</span>,\<br\>you will receive an amount of <span class='highlight'>&#165;"+interest+"</span>\<br\>in the year <span class='highlight'>"+year+"</span>.\<br\>";  
+    //Output Solution
+    solution = "If you deposit <span class='highlight'>&#165;"+p+"</span>\<br\>at an interest rate of <span class='highlight'>"+rate+"%</span>,\<br\>you will receive an amount of <span class='highlight'>&#165;"+interest+"</span>\<br\>in the year <span class='highlight'>"+year+"</span>.\<br\>";  
     document.getElementById("result").innerHTML = solution;
     return solution;   
 }
