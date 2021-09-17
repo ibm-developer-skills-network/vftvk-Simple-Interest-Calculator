fbf10306b18d8fad61c51a781e8f4a75518cdc1d
Hunter Faulkner <62822086+hrflkner@users.noreply.github.com>
2021-09-17 15:08:08 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    solution = "If you deposit <span class='highlight'>&#165;"+principal+"</span>\<br\>at an interest rate of <span class='highlight'>"+rate+"%</span>,\<br\>you will receive an amount of <span class='highlight'>&#165;"+interest+"</span>\<br\>in the year <span class='highlight'>"+year+"</span>.\<br\>";  
+    document.getElementById("result").innerHTML = solution;
+    return solution;   
 }
-        
