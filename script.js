b82cf730484204289e48a7c0114588e3c99e8cb8
Mayukh Dutta <mayukhere003@outlook.com>
2021-09-19 16:27:29 +0000

    # 2 : Minor changes as per assignment specifications

--- script.js
+++ script.js
@@ -2,23 +2,21 @@ function UpdateRate() {
     document.getElementById("rate_val").innerText = document.getElementById("rate").value+ "%";
 }
 
-// window.onload() = function() {
-    function PopulateYears() {
-        var years = document.getElementById("years");
-        for (var i = 1; i < 11; i++) {
-            var child = document.createElement("option");
-            child.innerText = i; child.setAttribute("value", i);
-            years.appendChild(child);
-        }
+function PopulateYears() {
+    var years = document.getElementById("years");
+    for (var i = 1; i < 11; i++) {
+        var child = document.createElement("option");
+        child.innerText = i; child.setAttribute("value", i);
+        years.appendChild(child);
     }
-// }
+}
 window.onload = PopulateYears;
 
 function Compute() {
     var principal = document.getElementById("principal");
     if ( parseFloat(principal.value) < 1.0 ) {
-        alert("Enter a positive number"); 
-        principal.focus;
+        alert("Enter a positive number!"); 
+        document.getElementById("principal").focus;
         return;
     }
     else {
@@ -27,7 +25,7 @@ function Compute() {
         var interest = principal.value * years * rate /100;
         var amount = parseFloat(principal.value) + parseFloat(interest);
         var year = new Date().getFullYear()+parseInt(years);
-        document.getElementById("result").innerHTML = "If you deposit <b>"+principal.value+"</b>, <br>at an interest rate of <b>"+rate+"</b>%, <br>You will receive <b>"+interest+"</b><br> in the year <b>"+year+"</b>.";
+        document.getElementById("result").innerHTML = "If you deposit <b style='background-color: yellow;'>"+principal.value+"</b>, <br>at an interest rate of <b style='background-color: yellow;'>"+rate+"</b>%, <br>You will receive <b style='background-color: yellow;'>"+interest+"</b><br> in the year <b style='background-color: yellow;'>"+year+"</b>.";
     }    
 }
         
