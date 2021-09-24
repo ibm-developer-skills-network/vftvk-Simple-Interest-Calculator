4254db0015f78e0d5fecec5161c88b8f5df52da8
nikkimasani <88842480+nikkimasani@users.noreply.github.com>
2021-09-24 09:13:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function compute() {
   // variable called "principal" and assigning it the value of the input element having id of "principal"
-  let principal = document.getElementById("principal").value;
+  var principal = document.getElementById("principal").value;
 
   // variable called "rate" and assigning it the value of the input element having id of "rate"
-  let rate = document.getElementById("rate").value;
+  var rate = document.getElementById("rate").value;
 
   // variable called "years" and assigning it the value of the input element having id of "years"
-  let years = document.getElementById("years").value;
+  var years = document.getElementById("years").value;
 
   // variable called "interest" and assigning it the value of principal * years * rate / 100
-  let interest = principal * years * rate / 100
+  var interest = principal * years * rate / 100
 
   // Convert number of year into actual year in future (ie, from now on) by using Date() constructor
-  let futureYearFromNow = new Date();
+  var year = new Date().getFullYear()+parseInt(years);
+
   futureYearFromNow.setFullYear(futureYearFromNow.getFullYear() + Number(years));
 
   // Get the reference to the element named "result" and show output of interest
@@ -21,10 +22,11 @@ function compute() {
 }
 
 // Function that reads value of range slider and shows the value in <span id="rangeOutput"> 
-function rangeSliderVal(e) {
-  e.preventDefault();
-  // Get live (current) value of range slider 
-  let rangeSliderValue = e.target.value;
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+};
 
   // Display the above read value into <span id="rangeOutput">  element
   document.getElementById("rangeOutput").innerHTML = `${rangeSliderValue}`;
