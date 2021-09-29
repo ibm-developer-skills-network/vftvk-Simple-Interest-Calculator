8727c50df441075c95f0eba8e802bdb64baf8fe3
Sathvik Anna <annasathvik08@gmail.com>
2021-09-29 17:41:31 +0530

    Update all files

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    if (checkPrincipal() == false) {
+        return;
+    }
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    var interest = principal * years * rate / 100;
+
+    var year = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>.\<br\>You will receive an amount of \<mark\>"+interest+"\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>\<br\>"
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval;
+}
+
+function checkPrincipal()
+{
+    var p = document.getElementById("principal");
+    if (p.value <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    return true;
+}
