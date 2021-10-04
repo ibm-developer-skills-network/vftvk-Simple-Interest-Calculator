db443502298cee72f9bac97aac7e9be192b840a2
Jennifer Johnson <75141440+Jennifer-Johnson1@users.noreply.github.com>
2021-10-04 16:55:37 -0500

    Updating JavaScript per Project Requirements
    Updating JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // Imported the HTML elements
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    result = document.getElementById("result");
+
+    // Calculate  the interest
+    interest = principal * years * rate / 100;
+
+    // Principal is positive number
+    if (principal < 1) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    // Calculate the year
+    thisYear = new Date().getFullYear();
+    futureYear = thisYear + Number(years);
+
+    // Display the result
+    result.innerHTML = "</br>If you deposit <mark>" + principal + "</mark>,</br> at an interest rate of <mark>" + rate + "</mark> %.</br> You will receive an amount of <mark>"  + interest + "</br></mark> in the year <mark>" + futureYear + "</mark>.";
 }
-        
+function rangeSlider() {
+    // Import HTML elements
+    rate = document.getElementById("rate").value;
+    rateNumber = document.getElementById("rate-number");
+
+    // Rate from the range slider in numbers
+    rateNumber.innerHTML = rate + " %";
+}    
