0abfacbb0b592f44a25e09ff5dfff82c27510f1b
Gabriel Palma <81444942+gabo1598@users.noreply.github.com>
2021-10-15 00:26:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    if ( principal != null && principal > 0){
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year<mark> "+year+"</mark>\<br\>"
+    }
+   else{
+        document.getElementById("principal").focus();
+        window.alert("Enter a positive number");
+       
+   }
+
     
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
