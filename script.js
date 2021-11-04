d9657b9ccb658eaeebe4d55d5f875e1b64d2205e
James Jawanda <james.jawanda@gmail.com>
2021-11-04 11:25:42 -0400

    test

--- script.js
+++ script.js
@@ -21,8 +21,7 @@ function SliderValue()
 var slider = document.getElementById("rate");
 var output = document.getElementById("rate_display");
 output.innerHTML = slider.value;
-
 slider.oninput = function() {
-  output.innerHTML = this.value;
+output.innerHTML = this.value;
 }
 }
