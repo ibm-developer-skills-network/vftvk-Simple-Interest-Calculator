892f28bf996076a1df240f7ea072aa8787dd5b61
bisrat-walle <87634885+bisrat-walle@users.noreply.github.com>
2021-11-12 08:46:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function compute()
     var p = document.getElementById("principal").value;
     var r = document.getElementById("rate").value;
     var y = document.getElementById("years").value;
-    var r = document.getElementById("result").value;
+    var r = document.getElementById("result");
     var i = p*r*y
     r.innerHTML = i
     
