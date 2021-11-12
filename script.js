550702cff2f7cb9766bd21bd428a608f830d520e
bisrat-walle <87634885+bisrat-walle@users.noreply.github.com>
2021-11-12 18:37:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ function compute()
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
     re = document.getElementById("result");
-    i = parseFloat(p)*parseFloat(r)*parseFloat(y)
-    re.innerHTML = i
-    
-    re.classList.add("format")
+    i = parseFloat(p)*parseFloat(r)*parseFloat(y)/100
+	if (isNaN(i)){
+		alert("Invalid Input")
+	} else{
+		document.getElementById("principal_").innerHTML = p
+		document.getElementById("rate_").innerHTML = r
+		document.getElementById("result").innerHTML = i
+		document.getElementById("year_").innerHTML = new Date().getFullYear()+parseFloat(y)
+		re.classList.add("format")
+		document.querySelector(".result").classList.remove('hide')
+	}
 }
-        
+
+function updateTextInput(val){
+	document.getElementById('per').innerHTML=(val+"%");
+}
