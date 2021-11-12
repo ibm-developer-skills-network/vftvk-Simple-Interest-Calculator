604b55b11da5be08d93caa8d7960eb3ddd0ef29b
bisrat-walle <87634885+bisrat-walle@users.noreply.github.com>
2021-11-12 08:40:52 +0300

    Completing the function

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var r = document.getElementById("result").value;
+    var i = p*r*y
+    r.innerHTML = i
+    
     
 }
         
