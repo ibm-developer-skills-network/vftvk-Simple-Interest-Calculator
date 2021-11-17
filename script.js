002fb9d7e2d3ec83719ddd2eaff03d2f51cd5d91
Linda Angulo Lopez <lindangulopez@example.com>
2021-11-17 16:00:25 +0000

    testing updateRate

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
+function compute() {
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate /100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
     p = document.getElementById("principal").value;
+
+    function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
     
+
 }
         
