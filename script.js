cec55cbf46b5f902e9f9a09fd94a8b645bc6dc89
tony <tonyelvis24@gmail.com>
2021-11-18 00:36:35 -0600

    Actualizando repo con cambios

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+///caculating process
 function compute()
 {
-    p = document.getElementById("principal").value;
+	console.log("hola");
     
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    if (principal <= 0) {
+        window.alert("Enter a positive number.");
+        document.getElementById("principal").focus();
+    }
+    else {
+    	document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark> \<br\>"
+    
+    }
+
+}
+
+//update rate dinamically
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
