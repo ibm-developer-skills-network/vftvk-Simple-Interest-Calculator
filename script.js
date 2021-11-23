a63f442b4261d2e3486eef962419dcced30fb61e
Raymond Lau <36816984+HeeyRay@users.noreply.github.com>
2021-11-23 15:33:17 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+var amount = principal + interest;
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+     
+    if(principal.value>=1){
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    }
+    else {
+        alert("Enter a positive number");
+        fname.focus();
 }
-        
+}
+
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
