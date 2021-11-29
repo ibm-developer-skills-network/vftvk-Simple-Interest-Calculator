84bf3b1a1036dc2110c4759fd7608c6fca081ceb
giftchule12 <giftchule@gmail.com>
2021-11-29 15:48:01 +0200

    Add compute function and showValue function
    Commit will add  compute function which set values and strings from user
    input, calculate interest and finally out the data.
    
    showValue function displays slider selected values.

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    // Set Values
+    amount = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    year = document.getElementById("years").value;
+
+    //Set string
+    amount_str = "If you deposit ";
+    rate_str = "at an interest rate of ";
+    interest_str = "You will receive an amount of ";
+    year_str = "in the year 2025"
+
+    //Calculate interest
+    interest =  amount * rate * year / 100;
     
+    // Output
+    document.getElementById("result").innerHTML = amount_str + amount + "<br/>"
+     + rate_str + rate + "<br/>"
+     + interest_str + interest + "<br/>"
+     + year_str;
+
+    console.log(interest);
+}
+
+function showValue(){
+    rate = document.getElementById("rate");
+    
+    document.getElementById("percent").innerHTML = rate.value;
+
 }
         
