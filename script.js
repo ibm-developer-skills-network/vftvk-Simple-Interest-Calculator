b663fc181bea0afce3f1ddf42267894cb1aab3a3
KPeterffy <Fulcrum11@users.noreply.github.com>
2021-12-01 17:44:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+%generate dropdown list
+
+var select = '';
+for (i=1;i<=100;i++){
+    select += '<option val=' + i + '>' + i + '</option>';
+}
+$('#years_select').html(select);
+
 function compute()
 {
     p = document.getElementById("principal").value;
