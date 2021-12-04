15b217c29981cdcadd5c33fc141787e2063504e2
GMalaconi <gmalaconi@protonmail.ch>
2021-12-04 00:57:05 -0300

    Layout changes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let compute = () => {
     }
 
     // Result user will see
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\> at an interest rate of " + rate + "%.\<br\> You will receive an amount of " + interest + ",\<br\> in the year " + year + "\<br\>";
+    document.getElementById("result").innerHTML = "\<br\>If you deposit " + principal + ",\<br\> at an interest rate of " + rate + "%.\<br\> You will receive an amount of " + interest + ",\<br\> in the year " + year + "\<br\>";
 }
 
 // Allowing users to visualize rate
