69bebbcd989f3d7a7c4f57ee06b8b1a0925f0580
giftchule12 <giftchule@gmail.com>
2021-12-05 10:41:19 +0200

    Add data validation and result output string

--- script.js
+++ script.js
@@ -3,24 +3,33 @@ function compute()
     // Set Values
     amount = document.getElementById("principal").value;
     rate = document.getElementById("rate").value;
-    year = document.getElementById("years").value;
-
-    //Set string
-    amount_str = "If you deposit ";
-    rate_str = "at an interest rate of ";
-    interest_str = "You will receive an amount of ";
-    year_str = "in the year 2025"
-
-    //Calculate interest
-    interest =  amount * rate * year / 100;
     
-    // Output
-    document.getElementById("result").innerHTML = amount_str + amount + "<br/>"
-     + rate_str + rate + "<br/>"
-     + interest_str + interest + "<br/>"
-     + year_str;
+    //parse to int
+    year = parseInt(document.getElementById("years").value);
+    
+    // Input validation
+    if (amount == ""){
+        alert("Please input amount!");
+    }
+    else{
+        //Set string
+        amount_str = "If you deposit ";
+        rate_str = "at an interest rate of ";
+        interest_str = "You will receive an amount of ";
+        year_str = "in the year "
 
-    console.log(interest);
+        //Year automation
+        date = new Date().getFullYear() + year;
+
+        //Calculate interest
+        interest =  amount * rate * year / 100;
+        
+        // Output
+        document.getElementById("result").innerHTML = amount_str + amount + "<br/>"
+        + rate_str + rate + "<br/>"
+        + interest_str + interest + "%" +"<br/>"
+        + year_str + date;
+    } 
 }
 
 function showValue(){
@@ -29,4 +38,6 @@ function showValue(){
     document.getElementById("percent").innerHTML = rate.value;
 
 }
+
+
         
