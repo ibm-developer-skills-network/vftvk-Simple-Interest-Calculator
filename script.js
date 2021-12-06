09162f56cbe24f7ac314ca67964e3d0200907d5c
GEEKY-EXPERTS <95287081+GEEKY-EXPERTS@users.noreply.github.com>
2021-12-06 18:01:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+	var principal = document.getElementById("principal").value;
+	var rate =  document.getElementById("rate_val").value;
+	var years = document.getElementById("years").value;
+	var interest = principal * years * rate/100;
+	var year = new Date().getFullYear()+parseInt(years);    
 }
-        
+
+ function validate() {
+         if( principal.value == "0" ) {
+            alert( "Enter a positive number" );
+            principal.focus() ;
+            return false;
+         }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+var n = "years";
+var y = Math.floor(n);            // whole years
+
+document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+y+"\<br\>"
