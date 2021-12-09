a45db2c420ff2984b4b28ad43874418351e78c16
javad jahed <jay.jahed@outlook.com>
2021-12-09 13:36:56 +0330

    Commit Test

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var principal = document.getElementById("principal").value;
+  if (!(principal > 0)) {
+    window.alert("Enter a positive number");
+    return;
+  }
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  if (year == 0) {
+    year = new Date().getFullYear() + parseInt(years);
+  }
+  var amount = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+
+  document.getElementById("result").innerHTML =
+    "If you deposit " +
+    principal +
+    ",<br>at an interest rate of " +
+    rate +
+    "%<br>You will receive an amount of " +
+    amount +
+    ",<br>in the year " +
+    year +
+    "<br>";
 }
-        
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
+}
