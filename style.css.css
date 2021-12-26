02905c01ed3b2370cfb162c1de60fbd53d53bdb8
des2154022 <89711925+des2154022@users.noreply.github.com>
2021-12-26 16:22:45 -0600

    Add files via upload

---
+++ style.css.css
@@ -0,0 +1,13 @@
+body {background-color:black;}
+h1{color:grey;
+font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+ 
+.maindiv{
+    background-color: white;
+    color: black;
+    width: 300px;
+    padding: 20px;
+    border-radius: 25px;
+    margin: auto;
+}
