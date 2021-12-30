9244904d69f0b0e2d582f284be1fc6b2443dd687
bcivils <bcivils@us.ibm.com>
2021-12-30 01:50:52 -0500

    Fine tune appearence, add error logic for invalid 'Principal' values, congfigure error clearing, configure calculation and results display

--- script.js
+++ script.js
@@ -2,13 +2,42 @@ const compute = () => {
     const principal_val = document.getElementById('principal').value;
     const rate_val = document.getElementById("rate").value;
     const year_select = document.getElementById("dropdown_val");
-    const year_val = year_select.getAttribute("data-value");
+    const years_val = year_select.getAttribute("data-value");
     console.log(`PRINCIPAL:  `, principal_val);
     console.log(`INTEREST RATE:  ${rate_val}`);
-    console.log(`YEAR:  ${year_val}`);
+    console.log(`YEAR:  ${years_val}`);
+    document.getElementById('error-principal').textContent = ``;
+    document.getElementById('compute-results').textContent = ``;
+
+    if (principal_val < 0) {
+        // document.getElementById('principal').appendChild(`
+        //     <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--number__invalid" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true"><path d="M8,1C4.2,1,1,4.2,1,8s3.2,7,7,7s7-3.1,7-7S11.9,1,8,1z M7.5,4h1v5h-1C7.5,9,7.5,4,7.5,4z M8,12.2	c-0.4,0-0.8-0.4-0.8-0.8s0.3-0.8,0.8-0.8c0.4,0,0.8,0.4,0.8,0.8S8.4,12.2,8,12.2z"></path><path d="M7.5,4h1v5h-1C7.5,9,7.5,4,7.5,4z M8,12.2c-0.4,0-0.8-0.4-0.8-0.8s0.3-0.8,0.8-0.8	c0.4,0,0.8,0.4,0.8,0.8S8.4,12.2,8,12.2z" data-icon-path="inner-path" opacity="0"></path></svg>
+        //     `);
+        // document.getElementById('num-item').setAttribute('class', 'data-invalid data-numberinput class=');
+        // const errPrincipal = document.createElement('div');
+        // errPrincipal.setAttribute('id', 'error-principal');
+        // errPrincipal.setAttribute('class', 'bx--form-requirement');
+        document.getElementById('error-principal').textContent = `Invalid value.  \"Amount\" must be greater than 0`;
+        // errPrincipal.textContent = `Invalid value.  \"Amount\" must be greater than 0`;
+        // document.getElementById('num-input').appendChild(errPrincipal);
+    } else {
+        const year = new Date().getFullYear()+parseInt(years_val);
+        const calc = calcIntrest(principal_val, rate_val, years_val)
+        console.log(`CALCULATION RESULT:  `, calc)
+        document.getElementById('compute-results').textContent = `
+            If you deposit ${principal_val}, at an interest rate of ${rate_val}%, you will
+            recieve an amount of $${calc.toFixed(2)} in the year ${year}`
+    }
+
+};
+
+const calcIntrest = (principal, ir, yrs) => {
+    return principal * ir * yrs / 100
 };
 
 const reset = () => {
+    document.getElementById('error-principal').textContent = ``;
+    document.getElementById('compute-results').textContent = ``;
     document.getElementById('principal').value = '';
     document.getElementById('slider-input-box').value = '0';
     document.getElementById('slide-thumb').style.left = '0%';
