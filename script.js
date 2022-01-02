b2bc0247dbbbf9230e0ffc8029657c4b5a1f9ba6
gustavo-fsouza <gustavo.fsouza@hotmail.com>
2022-01-02 14:47:13 -0300

    alteracoes javascript

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    capital = document.getElementById("capital").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    console.log(years);
+
+    currentYear = parseInt(new Date().getFullYear());
+    // converter string para int
+    years = parseInt(years);
+
+    years = currentYear + years;
+    rate = parseInt(rate);
+    capital = parseInt(capital);
+    amount = (capital * (rate/100)) + capital;
+
+    //criando uma nova div a partir da funcao
+    var NewDiv = document.createElement("div");
+    // inserindo código html na nova div
+    NewDiv.innerHTML = "<p>If you deposit: "+ capital +"</br>"
+        + "at an Interest rate of " + rate + "%</br>"
+        + "You will receive an amount of " + amount +"</br>"
+        + "in the year " + years +"</p>";
+    // criando vasriavel container para dizer onde quero que as divs sejam incluidas no html
+    const DivContainer = document.getElementById("DivContainer")
+    // adicionando essa div no body do documento
+    // document.body.appendChild(NewDiv);
+    DivContainer.appendChild(NewDiv)
 }
         
