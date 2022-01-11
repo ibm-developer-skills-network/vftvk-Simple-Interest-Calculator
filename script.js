49d5ad61846ef38fe56a7fb735c5d7b782d9a0ba
keymaker67 <69746377+keymaker67@users.noreply.github.com>
2022-01-12 00:24:55 +0330

    Consider It Done!

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let p = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = p * years * rate /100;
+    let year = new Date().getFullYear()+parseInt(years);
+    let result = document.getElementById("result")
+    if (p <= 0) {
+        document.getElementById("principal").value = "";
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } else {
+        result.innerHTML = `
+        If you deposit <span style="background-color: yellow;">${p}</span>,<br/>
+        at an interest rate of <span style="background-color: yellow;">${rate}%</span>.<br/>
+        You will receive an amount of <span style="background-color: yellow;">${interest}</span>,<br/>
+        in the year <span style="background-color: yellow;">${year}</span>
+        `    
+    };
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
