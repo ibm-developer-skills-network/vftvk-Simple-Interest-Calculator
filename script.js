a33aada2fef3a4af50d7dd85c7b3ec0a94b08bd7
maxfun-astar <89070157+maxfun-astar@users.noreply.github.com>
2022-01-18 00:34:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,6 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval;
 }
 
-function checkPrincipal()
-{
-    var principal = document.getElementById("principal").value;
-
-    if (principal <= 0) {
-        alert("Enter a positive Number");
-        document.getElementById("principal").focus()
-    } 
-}
-
 function compute()
 {
     var principal = document.getElementById("principal").value;
@@ -21,7 +11,12 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-
+    
+    if (principal <= 0) {
+        alert("Enter a positive Number");
+        document.getElementById("principal").focus()
+    } else {
     document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>\<br\>You will receive an amount of \<mark\>"+interest+"\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>"
-}
+    }
+ }
         
