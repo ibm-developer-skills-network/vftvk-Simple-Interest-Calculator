ed2bf40d3d060f4a5bfa8af4f7051de7b04f2313
maxfun-astar <89070157+maxfun-astar@users.noreply.github.com>
2022-01-18 00:47:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function compute()
     if (principal <= 0) {
         alert("Enter a positive Number");
         document.getElementById("principal").focus()
-    } else {
+    } else  {
     document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>\<br\>You will receive an amount of \<mark\>"+interest+"\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>"
     }
  }
