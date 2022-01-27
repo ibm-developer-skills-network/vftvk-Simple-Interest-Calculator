8d3c1017ccd48c7772bd4eec349319ff39cf425c
rudraburgula <rudraburgula@gmail.com>
2022-01-27 03:38:33 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    if (principal == 0 || principal < 0) {
+        alert(`You should see an alert "Enter a positive number".`)
+    }
+    else {
+        var d = `<p>If you deposit ${principal},<br/>
+    at an interest rate of ${rate}.<br/>
+    You will receive an amount of ${interest},<br/>
+    in the year ${year}.</p>`
+        document.getElementById("result").innerHTML = d;
+    }
+
 }
-        
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+
+
+}
