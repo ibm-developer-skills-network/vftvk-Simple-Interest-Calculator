28df77d8f8ad6758d7019c31df6e3938a712536c
mohitkb257 <98774637+mohitkb257@users.noreply.github.com>
2022-01-31 15:37:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
-    for(var i=1; i<=50; i++){
-    var select = document.getElementById("years");
-    var option = document.createElement("OPTION");
-	select.options.add(option);
-	option.text = i;
-	option.value = i;
-}
-}
-    
+	
 }
         
