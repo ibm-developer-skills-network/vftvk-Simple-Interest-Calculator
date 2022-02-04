0c52be55cbc202f8c9b21c14fb8f6a8a44367b62
GayaKumaran <98042480+GayaKumaran@users.noreply.github.com>
2022-02-04 18:46:49 -0500

    Update script.js
    Aded new changes

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+function updateRate() 
+{
+    // Gets the correct range selected by user from the SPAN and displays in the page
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //Calculates the amount the return the valid year
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var Amount = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%,\<br\>You will receive an amount of "+Amount+",\<br\>in the year "+year+"\<br\>"
 }
+
+function checkdata()
+{
+    //create references to the input elements we wish to validate
+    var amtcheck = document.getElementById("principal").value;
+
+    //Check if amount field is zero
+    if(amtcheck == 0 )
+    {
         
+            alert("Please enter the positive number");
+            amtcheck.focus();
+            return false;
+          
+      
+    }
+    return true;
+}
+
+//Just for my reference
+function myFunction(val) {
+    alert("The input value has changed. The new value is: " + val);
+  }
