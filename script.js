824de5c7b7fba026c363ecbb7be248b00b908bc3
shuubham <96609435+shubham1091@users.noreply.github.com>
2022-02-11 15:19:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
 function compute()
 {
     p = document.getElementById("principal").value;
     
+    
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
