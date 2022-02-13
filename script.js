e336386cf06da0ac4f603efdb48f21a1766e399a
shameekh <57168902+codergypsy@users.noreply.github.com>
2022-02-13 21:14:03 +0300

    Completed The Exercise

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+function showVal(newVal){
+    document.getElementById("rs_value").innerHTML= newVal + "%";
+}
+
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    rate = document.getElementById("rs_range").value;
+    noy = document.getElementById("nofy").value;
+    ans = (p*rate*noy)/100;
+    y = new Date().getFullYear();
+    ey = y + parseInt(noy);
+    document.getElementById("result").innerHTML="If you deposit " + p + " at an interest rate of " + rate + "%. You will receive an amount of " + ans + ", in the year " + ey;
+      
 }
-        
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val; 
+  } 
+  
+  
