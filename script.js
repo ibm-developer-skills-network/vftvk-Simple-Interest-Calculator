6096d534c720d32b1f141796b04f9dd9b759d811
Mostafa Safwat <36309847+010101mostafa@users.noreply.github.com>
2022-02-15 03:19:45 +0200

    javaScript
    add the code to calculate the Amount

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
-}
-        
+    a = document.getElementById("Amount").value;
+    Rate=document.getElementById("Rate").value;
+    years=parseInt(document.getElementById("years").value);
+    result= a*Rate*years/100;
+    inyear=(new Date()).getFullYear()+years;
+    document.getElementById("result").innerHTML="<br>If you deposit <mark>"+a+"</mark>,<br>"+
+        "at an interest rate of <mark>"+Rate+"\%</mark><br>"+
+        "You will receive an amount of <mark>"+result+"</mark>,<br>"+
+        "in the year <mark>"+inyear+"</mark>";
+     
+}  
