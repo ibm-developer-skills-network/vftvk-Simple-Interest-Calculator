e31ef275652b4df5de02aaeed40c5fc974f0c227
Shah Azharuddin <92333469+ShahAzharuddin@users.noreply.github.com>
2022-02-19 23:22:16 +0530

    Update script.js
    updated function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+//Funtion to compute rate of interest
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
-        
+//Funtion to update rate of interest dynamically on slider
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
