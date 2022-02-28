1f65439c9805b9e1346a4dde93e0726d03816c6d
sabakhann64 <100476160+sabakhann64@users.noreply.github.com>
2022-02-28 09:25:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    var SI = p*r*t;
+    document.getElementById("result").innerHTML = p;
     
 }
         
