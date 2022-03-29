f1db7d579f6ff715e16218b13a1b851fa1a23382
astridsawatzky <astrid.sawatzky@gmail.com>
2022-03-29 13:11:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    //Get the values and calculate 
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
+
 }
-        
+
+//update ther ate value
+function getSliderValue() {
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+}
+
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
