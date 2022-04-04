4df0019a6636dfa7f9e6341000632ff557cb8c26
Amy <102967075+amymaybe@users.noreply.github.com>
2022-04-04 20:12:13 +0100

    Update script.js
    Updated and additions made as per requirement.

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseFloat(document.getElementById("years").value);
+    var result = principal * years * rate /100;
+    var years_modified = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result").innerHTML = "If you deposit <mark>"
+   + principal + "</mark>, <br/> at an interest rate of <mark>"
+   + rate +"%.</mark><br/> You will receive an amount of <mark>" + result +
+   "</mark>,<br/>in the year <mark>" + years_modified + ".</mark><br/>";
 }
-        
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+function checkAmount() 
+{
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) 
+    {
+        alert("Enter a non-zero or a positive number");
+        document.getElementById("principal").focus();
+    }
+}      
