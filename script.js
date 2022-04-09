9d41e1511402e3bc6fe5093e65b53e25d28e99ea
parikhparth23 <parikhparth23@gmail.com>
2022-04-09 16:17:52 -0700

    final changes

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    // document.getElementById("demo").innerHTML = "Hello World";
+
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rateInput").value;
+    var ap = r/100;
+    var t = document.getElementById("years").value;
+    var calculateYear = parseInt(new Date().getFullYear()) + parseInt(t);
+
+    if (p == null || p == "") {
+        alert("Please fill 'Amount' to calculate Simple Iterest.");
+        return false;
+    }
+
+    var total;
+    total = p * ap * parseInt(t)
+
+    document.getElementById("demo").innerHTML = "If you deposit total " + p + ",<br>at an interest rate of " + r + "% per year,<br>You will receive an amount of " + total + ",<br>in the year " + calculateYear + "."
 }
-        
