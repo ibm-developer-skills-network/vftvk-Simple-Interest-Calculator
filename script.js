d4734e5684c6a1bf4ead99d00f393fd768f5dad7
parikhparth23 <parikhparth23@gmail.com>
2022-04-09 16:55:20 -0700

    changes based on the submission

--- script.js
+++ script.js
@@ -9,14 +9,17 @@ function compute() {
 
     if (p == null || p == "") {
         alert("Please fill 'Amount' to calculate Simple Iterest.");
+        document.getElementById("principal").focus();
         return false;
-    } else if (p == 0){
+    } else if (p == 0 || p < 0){
         alert("Please enter Principal amount greater than 0.");
+        document.getElementById("principal").focus();
         return false;
     }
 
     var total;
     total = p * ap * parseInt(t)
 
-    document.getElementById("demo").innerHTML = "If you deposit total " + p + ",<br>at an interest rate of " + r + "% per year,<br>You will receive an amount of " + total + ",<br>in the year " + calculateYear + "."
+    document.getElementById("demo").innerHTML = "If you deposit total <mark>" + p + "</mark>,<br>at an interest rate of <mark>" + r + "%</mark> per year,<br>You will receive an amount of <mark>" + total + "</mark>,<br>in the year <mark>" + calculateYear + "</mark>."
 }
+  
