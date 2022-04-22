825418b82e5e828c6b44fcfce83a3c4a2fb1effe
Taito Kantomaa <taitosamuli@gmail.com>
2022-04-22 10:37:10 +0300

    code and style

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate/100;
+    var amount = interest;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    if (principal === '' || principal <= 0) {
+        alert("Enter positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+
+    document.getElementById("result").innerHTML = "If you deposit \<mark\>" + principal + "\</mark\>\<br\>at an interest rate of \<mark\>" 
+    + rate + "%\</mark\> \<br\>You will receive an amount of \<mark\>" + amount + 
+    "\</mark\>, \<br\>in the year \<mark\>" + year + "\</mark\>\<br\>";
+
 }
         
+function updateRate() {
+
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+
+}
