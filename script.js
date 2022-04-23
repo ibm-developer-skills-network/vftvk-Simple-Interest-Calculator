42384ec3e6e9bbb9070d606e7cd0cf62ded4af13
Vaishali Kannan <vaishalikannan92@gmail.com>
2022-04-24 01:04:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    if (principal < 1){
+		alert("Enter a positive number");
+		principal.focus;
+	}
+	if (principal == ""){
+		alert("Enter a positive number");
+		principal.focus;
+	}
+	else {
+		var interest = principal * years * rate / 100;  
+    	var year = new Date().getFullYear()+parseInt(years);  
+		document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,</br> at an interest rate of <mark>" + rate + "</mark>%. </br> You will recieve an amount of <mark>" + interest + "</mark>, </br> in the year <mark>" + year + "</mark>"
+	}
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
