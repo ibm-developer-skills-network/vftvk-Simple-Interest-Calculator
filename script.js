ec1b2d7f44255781c03cc3d11f56ce14b2d18694
yodac89 <hannah_collins@live.co.uk>
2022-04-28 12:17:45 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,64 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+function compute() 
+
+{ //to get values 
+
+    var principal = document.getElementById("principal").value; 
+
+    var rate = document.getElementById("rate").value; 
+
+    var years = document.getElementById("years").value; 
+
+     
+
+    //to calculate interest 
+
+    var interest = principal * years * rate /100; 
+
+    var year = new Date().getFullYear()+parseInt(years); 
+
+     
+
+    if(principal == undefined || principal <= 0){ 
+
+        alert("Enter a Positive Number"); 
+
+        document.getElementById("principal").focus(); 
+
+         
+
+        return 
+
+    }else{ 
+
+        // to display result 
+
+        var inr = "If you deposit <mark>" + principal +",</mark><br>"; 
+
+        var ra = "at an interest rate of <mark>"+ rate +" %,</mark><br>"; 
+
+        var am="You will receive an amount of <mark>" +interest + ",</mark><br>" 
+
+        var ye ="in the year <mark>"+year +"</mark>"; 
+
+ 
+ 
+
+        document.getElementById("result").innerHTML=inr+ra+am+ye; 
+
+ 
+ 
+
+    } 
+
+     
+
+} 
+
+//to change slider value display 
+
+function getSliderValue() { 
+
+    document.getElementById("rate_val").innerHTML=document.getElementById("rate").value; 
+
+} 
         
