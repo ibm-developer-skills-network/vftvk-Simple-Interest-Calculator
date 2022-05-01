6d6c721e04566d303b8cb352e6c7cb28e6728815
elrate99 <vadim300012@gmail.com>
2022-05-01 22:32:51 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+
+    //checking for a negative number
+    if (principal <= 0 ){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else {
+        document.getElementById("result").innerHTML="If you deposit\<span\> "+principal+"\</span\>,\<br\>at an interest rate of \<span\>"+rate+"\</span\>%\<br\>You will receive an amount of \<span\>"+interest+"\</span\>,\<br\>in the year \<span\>"+year+"\</span\>\<br\>";
+    }
 }
-        
