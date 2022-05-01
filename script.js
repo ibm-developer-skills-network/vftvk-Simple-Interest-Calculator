7b460dcedde67a0b12acdb41e888ba5cc27a6f08
symbiotik_007 <69918011+symbiotik007@users.noreply.github.com>
2022-05-02 07:27:10 +0800

    update
    update

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  if (principal == "") {
+    alert("El valor no puede estar vacío");
+    document.getElementById("principal").focus();
+    return false;
+  } else {
+    if (principal <= 0) {
+      alert("Ingresa un numero positivo");
+      document.getElementById("principal").focus();
+      return false;
+    }
+  }
+
+  var anos = new Date().getFullYear();
+  var future = parseint(anos,10) + parseInt(years,10);
+
+  var interest = principal * rate * years / 100;
+  var result = "Si depositas" + principal + "en un interes de" + rate + "tu recibiras la suma de " + interest + "in the year " + future;
+  document.getElementById("result").innerHTML=result;
 }
-        
+
+
+
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
+  <select onchange="myFunction()"></select>;
+}
