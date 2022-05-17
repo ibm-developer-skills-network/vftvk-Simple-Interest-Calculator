37d7b57e340f3bbfe83a0fef9a23d361eef16a60
Hector5280g <hectorgarcia342@yahoo.com>
2022-05-17 01:30:08 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    var amount = document.getElementById("Amount").value;
+    var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = amount * years * rate/100;
+    var interest = principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years);
-    var result =document.getElementById("result");
-    result.innerHTML=`If you deposit <mark>${amount}</mark>,<br>at an interest rate of <mark>${rate}%</mark>.<br>You will receive an amount of <mark>${interest}</mark>,<br>in the year <mark>${year}</mark>`;
-
-
-}
-
-
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval+"%";
-}
-
-function warning()
-{
-    var amount = document.getElementById("Amount").value;
-    if (amount<=0){
-        alert("Enter a positive number");
-        document.getElementById("Amount").focus();
-
+ 
+    if(principal<=0)    
+    {
+        window.alert("Enter a positive number")
+        document.getElementById("principal").focus();
     }
-
-
-} 
+ 
+    else
+   
+    document.getElementById("result").innerHTML="If you deposit "+"\<mark>"+principal + "\</mark>"+",\<br\>at an interest rate of "+"\<mark>"+rate+"\</mark>"+"%\<br\>You will receive an amount of $"+"\<mark>"+interest+"\</mark>"+",\<br\>in the year "+"\<mark>"+year+"\</mark>"+"\<br\>"
+   
+}
+ 
+ 
+function updateRate(val)
+{
+    document.getElementById("rate_val").innerText=val + "%";
+}
