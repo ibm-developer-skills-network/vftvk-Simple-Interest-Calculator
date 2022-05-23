525e81503b10b0ca24a2ca80e1b966e0d50bcc45
Willie Joseph <chefwilliejoseph@gmail.com>
2022-05-23 01:00:11 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //this is to get the value of the calculator
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + years;
     
+    
+    //this is to show the output text after calculating
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + year + "</mark>";
+}
+
+
+//for the rate dynamic rate value
+function getSliderValue() {
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
+}
+
+
+
+//this is to avoid the negative value and show an alert
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
 }
         
