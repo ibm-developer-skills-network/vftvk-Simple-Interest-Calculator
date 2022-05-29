c133eeab7c45ff8b9ef59addab40ce7d96e5a02a
j-hallen <106459590+j-hallen@users.noreply.github.com>
2022-05-29 16:05:30 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    if (principalValid()) {
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    }
 }
-        
+
+function updateRate()
+{
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateVal+"%";
+}
+
+function principalValid() {
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+      alert("Enter a positive number");
+      return false;
+    } else {
+        return true;
+    }
+  }
