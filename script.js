20f54781a2564546a7ec4ad0d8c5242ab87ebf63
Zeenat Shareef <zeenatshareef1@gmail.com>
2022-06-09 10:49:05 +0530

    final project course 2

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+        
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").valuel;
+var years = document.getElementById("years").valuel;
+var interest = principal * years * rate/100;
+var year = new Date().getFullYear() + parseInt(years);
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
-        
+ function checkdata()
+ {
+     if (principal==0){
+         alert("Enter a positive number");
+     }
+     
+     else if (principal<0){
+        alert("Enter a positive number");
+     }
+     else{
+     return (true);
+     }
+ }
+}
