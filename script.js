f94081ccaf0a9d0eaf7699ed974f5cbf406e8f16
ZeenatShareef <105618242+ZeenatShareef@users.noreply.github.com>
2022-06-10 23:50:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
         
 function compute()
 {
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").valuel;
-var years = document.getElementById("years").valuel;
-var interest = principal * years * rate/100;
-var year = new Date().getFullYear() + parseInt(years);
-
-function updateRate()
+    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+      
+}
+function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
- function checkdata()
- {
-     if (principal==0){
-         alert("Enter a positive number");
-     }
-     
-     else if (principal<0){
+
+function checkdata()
+{
+    var principal = document.getElementById("principal").value;
+    if (principal.value == 0)
+    {
         alert("Enter a positive number");
-     }
-     else{
-     return (true);
-     }
- }
+        username.focus();
+        return false;
+
+    }
+    if(principal.value < 0 )
+    {
+        alert("Enter a positive number");
+        username.focus();
+        return false;
+    }
+    return true;
 }
