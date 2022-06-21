037cbfcc8821c9888a9533729ea9a410c6bdc170
Chanuka Ranathunga <chamithroxmpc@gmail.com>
2022-06-22 00:33:03 +0530

    Final Project has been done!
    Final Project has been done!

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //var p = document.getElementById("principal").value;
+
+    var principal = document.getElementById("principal").value;
+
+    if(principal<=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else{
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate/100;
+        var year = new Date().getFullYear()+parseInt(years);
     
+    document.getElementById("result").innerHTML="\<mark\>\<br\>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>\</mark\>";
+    }
+    
+
 }
-        
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value; 
+    document.getElementById("rate_val").innerText=rateval;    
+
+}
+    
