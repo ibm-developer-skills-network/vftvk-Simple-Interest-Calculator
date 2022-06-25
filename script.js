7de30471c6134a82198a145674951ea195b866a0
Hassan Samo <hassansamo609@gmail.com>
2022-06-25 19:59:52 +0500

    <commit message>

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+let principal = document.getElementById("principal");
+let rate = document.getElementById("rate");
+let years = document.getElementById("years");
+function checkData() {
+    console.log(principal.value.length);
+    if (principal.value <= 0 | principal.value.length == 0) {
+        alert("Enter a positive number");
+        document.getElementById('result').innerText = '';
+        return false;
+    }
+    compute();
+    return true;
 }
-        
+function compute() {
+    let result = principal.value * (rate.value / 100) * years.value;
+    years = new Date().getFullYear() + parseInt(years.value);
+    let template = `<p>If you deposit <span class="highlight">${principal.value}</span>,<br>at an interest rate of <span class="highlight">${rate.value}%</span>.<br>You will receive an amount of <span class="highlight">${result}</span>,<br>    in the year <span class="highlight">${years}</span></p>`;
+    document.getElementById('result').innerHTML = template;
+}
