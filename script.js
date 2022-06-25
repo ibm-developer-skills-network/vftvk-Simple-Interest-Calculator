8e47a113081daed216eb8de4d589c885f4be2b52
Hassan Samo <hassansamo609@gmail.com>
2022-06-25 20:32:20 +0500

    <commit message>

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let principal = document.getElementById("principal");
-let rate = document.getElementById("rate");
-let years = document.getElementById("years");
+var principal = document.getElementById("principal");
+var rate = document.getElementById("rate");
+var years = document.getElementById("years");
 function checkData() {
-    console.log(principal.value.length);
     if (principal.value <= 0 | principal.value.length == 0) {
         alert("Enter a positive number");
         document.getElementById('result').innerText = '';
         return false;
     }
     compute();
-    return true;
 }
 function compute() {
-    let result = principal.value * (rate.value / 100) * years.value;
-    years = new Date().getFullYear() + parseInt(years.value);
-    let template = `<p>If you deposit <span class="highlight">${principal.value}</span>,<br>at an interest rate of <span class="highlight">${rate.value}%</span>.<br>You will receive an amount of <span class="highlight">${result}</span>,<br>    in the year <span class="highlight">${years}</span></p>`;
+    var result = Math.round(principal.value * (rate.value / 100) * years.value);
+    var result_year = new Date().getFullYear() + parseInt(years.value);
+    console.log(typeof(result), result);
+    console.log(typeof(result_year), result_year);
+    var template = `<p>If you deposit <span class="highlight">${principal.value}</span>,<br>at an interest rate of <span class="highlight">${rate.value}%</span>.<br>You will receive an amount of <span class="highlight">${result}</span>,<br>    in the year <span class="highlight">${result_year}</span></p>`;
     document.getElementById('result').innerHTML = template;
 }
