5c5d3d458b1c06619a03341d7922848fd242f9ed
Hassan Samo <hassansamo609@gmail.com>
2022-06-26 00:43:24 +0500

    changes

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ var years = document.getElementById("years");
 function checkData() {
     if (principal.value <= 0 | principal.value.length == 0) {
         alert("Enter a positive number");
-        document.getElementById('result').innerText = '';
+        document.getElementById('result').innerHTML = '';
+        principal.focus();
         return false;
     }
     compute();
 }
 function compute() {
-    var result = Math.round(principal.value * (rate.value / 100) * years.value);
-    var result_year = new Date().getFullYear() + parseInt(years.value);
-    console.log(typeof(result), result);
-    console.log(typeof(result_year), result_year);
-    var template = `<p>If you deposit <span class="highlight">${principal.value}</span>,<br>at an interest rate of <span class="highlight">${rate.value}%</span>.<br>You will receive an amount of <span class="highlight">${result}</span>,<br>    in the year <span class="highlight">${result_year}</span></p>`;
+    var interest = Math.round(principal.value * years.value * rate.value / 100);
+    var year = new Date().getFullYear() + parseInt(years.value);
+    var template = `If you deposit <span class="highlight">${principal.value}</span>,<br>at an interest rate of <span class="highlight">${rate.value}%</span>.<br>You will receive an amount of <span class="highlight">${interest}</span>,<br>    in the year <span class="highlight">${year}</span>`;
     document.getElementById('result').innerHTML = template;
 }
