59e48e23f8bbbe793409e5b272761628a502643f
KJ <kiki_jones@outlook.com>
2022-08-30 19:16:49 -0700

    Project testing index html, script js, style css

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+function updateRate()
 {
-    p = document.getElementById("principal").value;
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+function onchange(updateRate);
+{
+}
+
+function compute()
+{
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of <mark>"+rate+"%\<br\></mark>You will receive an amount of <mark> "+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+}
+if (principal < 1){
+    alert("Enter a positive number");
+        principal.focus();
     
 }
-        
