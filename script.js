3ec1a3dd359b679997a47233017a61c2096735ff
Luis210380 <ing.lcarvajal@gmail.com>
2022-10-24 19:23:38 +0100

    changed by Luis 1923

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+   var principal = document.getElementById("principal").value;
+   var rate = document.getElementById("rate").value;
+   var years = document.getElementById("years").value;
+   var interest = principal * years * rate /100;
+   var year = new Date().getFullYear()+parseInt(years);
+    if(principal <= 0){
+        alert("Enter a positive number");
+    }
+    else{
+        document.getElementById("principalResult").innerText=principal;
+        document.getElementById("rateResult").innerText=rate;
+        document.getElementById("yearsResult").innerText=year;
+        document.getElementById("result").innerText=interest;
+    } 
 }
-        
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
