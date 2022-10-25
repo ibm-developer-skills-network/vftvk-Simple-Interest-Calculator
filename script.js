236b0b500c87531f759d806e0073ba44d31efa8d
Sean <smcolligan00@gmail.com>
2022-10-25 08:06:39 -0700

    Finished interest rate calculator
    Ready for review

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //alert("compute");
+
+    var principal = document.getElementById("principal").value;
+
+    if (principal > 0)
+    {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+
+        var str = "<p>";
+        str += "If you deposit <mark>$ " + principal; 
+        str += "</mark> at an interest rate of <mark>" + rate + "%</mark>,";
+        str += " you will receive an amount of <mark>$" + interest;
+        str += "</mark> in the year <mark>" +  year + "</mark>."; 
+        str += "</p>"
+        document.getElementById("result").innerHTML = str;
+    }
+    else {
+            alert("Enter a positive number for the Amount");
+    }
 }
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+
         
