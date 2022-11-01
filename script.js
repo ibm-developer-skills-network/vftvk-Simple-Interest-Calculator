efcd3ca4b67b94610894956c09f7a6c29ecd4947
mubeen3116 <117159823+mubeen3116@users.noreply.github.com>
2022-11-01 15:08:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    n = document.getElementById("n").value;
-    r = document.getElementById("r").value;
+    n = document.getElementById("rate").value;
+    r = document.getElementById("years").value;
     result = document.getElementById("result");
                 
     result.innerHTML = "The interest is " + (p*n*r/100);
