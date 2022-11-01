8a36e3e0c6821fc1e7dee4a1d6684d50f50b3b1e
mubeen3116 <117159823+mubeen3116@users.noreply.github.com>
2022-11-01 14:51:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    n = document.getElementById("n").value;
+    r = document.getElementById("r").value;
+    result = document.getElementById("result");
+                
+    result.innerHTML = "The interest is " + (p*n*r/100);
+    
     
 }
         
