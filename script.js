37d079e2106e1cbc64b53646246b45542073817e
baggel0815 <119439338+baggel0815@users.noreply.github.com>
2022-11-30 10:04:54 +0100

    Update script.js
    final exam

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var result = document.getElementById("result");
+    result.innerHTML="If you deposit <b>"+principal+"</b>,<br\>at an interest rate of <b>"+rate+"%</b><br\>You will receive an amount of <b>"+interest+",</b>\<br\>in the year <b>"+year+"</b><br\>";
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validate()
+{
+    var principal = document.getElementById("principal");
+    if ( principal.value <= 0)
+    {   
+       alert("Enter a positive number");
+       principal.focus();
+    }
+}
