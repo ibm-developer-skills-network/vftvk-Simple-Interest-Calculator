8522f6fef760f106ffdb8c32fc8fca1209627e41
Kuavo M <Kuavo10@gmail.com>
2022-12-11 00:02:03 -0600

    updates the entire Calculator

--- script.js
+++ script.js
@@ -11,5 +11,8 @@ function compute()
 function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
+
+    var result = document.getElementById("result").innerHTML = ("If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year + "\<br\>");
+
 }
         
