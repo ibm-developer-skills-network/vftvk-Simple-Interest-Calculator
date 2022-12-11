60b556ae8f529b496e62344abc2c06dd591e7c10
Kuavo M <Kuavo10@gmail.com>
2022-12-10 23:52:06 -0600

    updates the OG interest calculator

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (principal * rate * years) / 100;
+
+    var year = new.Date().getFullYear() + parseInt(years);    
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
         
