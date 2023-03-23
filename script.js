b8f8df49cb87fdae24d09af37060500dd3c1b7f3
OhenebaEvans <antwiohenebaevans2002@gmail.com>
2023-03-23 01:20:06 +0000

    Improve accuracy on computation of final results

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y= document.getElementById("years").value;
+
+
+        var rr =  r/100;
+    var r= p * rr * y;
+    alert("The interest is "+r); 
+  
+  
     
 }
         
