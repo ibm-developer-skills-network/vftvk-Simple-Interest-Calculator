c6b34e4aa50e45e0b6c65bd2d321e0004098beb6
ahmedBasas <a.basas@hotmail.com>
2023-04-02 03:59:04 -0400

    Changes made

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function calculate() {
+    var p = parseFloat(document.getElementById("p").value);
+    var r = parseFloat(document.getElementById("r").value);
+    var t = parseFloat(document.getElementById("t").value);
+    var futureYear = new Date().getFullYear() + t;
+    var result = document.getElementById("result");
+    var interest = (p * t * r / 100);
+    result.innerHTML = "The interest is " + interest.toFixed(2);
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("r").value;
+    document.getElementById("rate_val").innerText = rateval+"%";
+}
