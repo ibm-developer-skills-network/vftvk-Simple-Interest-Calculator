8f18110108f6b1cb908a10e074c551bc6f556472
Rian <rhiannon.illustration@gmail.com>
2023-06-04 01:05:15 -0400

    Updating html, css, js files
    Signed-off-by: Rian <rhiannon.illustration@gmail.com>

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value; 
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var amount = parseInt(principal) + parseFloat(interest);
+    var result = document.getElementById("result");
     
+    if (principal <= 0) {
+        alert('Please enter a positive number!');
+        document.getElementById("principal").focus();
+    }
+    else {
+        result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+    } 
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
